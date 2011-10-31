4062f24de70353346d6cbee8759424fdeb3ff95e
magnuskragelund <magnuskragelund@gmail.com>
2011-10-31 15:44:33 +0100

    corrected spelling mistake in CatalogViewModel

--- Filmster.Web/Models/CatalogViewModel.cs
+++ Filmster.Web/Models/CatalogViewModel.cs
@@ -8,7 +8,7 @@ namespace Filmster.Web.Models
     {
         public List<Movie> Movies { get; set; }
         public string SelectedValue { get; set; }
-        public string[] Alpabet
+        public string[] Alphabet
         {
             get { return "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,Æ,Ø,Å".Split(','); }
         }
