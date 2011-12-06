5eda70a5b816c2700307c8d91412e338521fdfba
magnuskragelund <magnuskragelund@gmail.com>
2011-12-06 21:42:56 +0100

    fixes for ituens crawler

--- Filmster.Crawler/Program.cs
+++ Filmster.Crawler/Program.cs
@@ -50,8 +50,8 @@ namespace Filmster.Crawler
             Logger.Log("Initiating Crawl");
             ThreadPool.SetMinThreads(40, 40);
             ThreadPool.SetMaxThreads(120, 120);
-            new CdonCrawler().Start();
             new ItunesCrawler().Start();
+            new CdonCrawler().Start();
             new VoddlerCrawler().Start();
             new HeadwebCrawler().Start();
             new ViaPlayCrawler().Start();
